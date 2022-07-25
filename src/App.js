05a74ceab114193e3fc7f675a5aaf61f121fd4f1
Son Nguyen <nguyencaoson8112k@gmail.com>
2022-07-25 13:54:17 +0700

    protect rounter

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { AuthContextProvider } from "./context/AuthContext";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Account from "./pages/Account";
+import PretecteRoute from "./Components/PretecteRoute";
 
 function App() {
   return (
@@ -15,7 +16,11 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path="/login" element={<Login/>}/>
           <Route path="/signup" element={<Signup/>}/>
-          <Route path="/account" element={<Account/>}/>
+          <Route path="/account" element={
+          <PretecteRoute>
+            <Account/>
+          </PretecteRoute>
+          }/>
 
         </Routes>
       </AuthContextProvider>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link,useNavigate } from 'react-router-dom'
 import { UserAuth } from '../context/AuthContext'
 
 const Navbar = () => {
-  const {user,logOut} = UserAuth()
+  const { user, logOut } = UserAuth()
+  const navigate = useNavigate()
+  const handleLogOUT = async () => {
+    try {
+      await logOut()
+      navigate('/')
+
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
   console.log(user);
   return (
     <div className='flex justify-items-center justify-between p-4 z-[999] w-full fixed'>
@@ -13,15 +24,24 @@ const Navbar = () => {
           <h1 className='text-white m-1 text-3xl'> SUB</h1>
         </div>
       </Link>
-      <div>
-        <Link to='/login'>
-          <button className='bg-red-600 text-black px-4 py-3 rounded'>Sign In</button>
-        </Link>
-        <Link to='/signup'>
-          <button className=' text-white px-4 py-3'>Sign Up</button>
-        </Link>
+      {user?.email ?
+        <div>
+          <Link to='/account'>
+            <button className='bg-red-600 text-black px-4 py-3 rounded'>Account</button>
+          </Link>
+          
+            <button onClick={handleLogOUT} className=' text-white px-4 py-3'>Log Out </button>
+        </div>
+        : <div>
+          <Link to='/login'>
+            <button className='bg-red-600 text-black px-4 py-3 rounded'>Sign In</button>
+          </Link>
+          <Link to='/signup'>
+            <button className=' text-white px-4 py-3'>Sign Up</button>
+          </Link>
 
-      </div>
+        </div>
+      }
     </div>
   )
 }
